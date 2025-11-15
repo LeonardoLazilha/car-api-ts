09e15502b4ee57c4b422a3314bff7275ad4166f9
Leonardo Lazilha <leonardolazilha@example.com>
2025-11-15 17:13:59 -0300

    feat: list all cars route

--- src/controllers/car-controller.ts
+++ src/controllers/car-controller.ts
@@ -9,3 +9,12 @@ export const create = async (req: Request, res: Response): Promise<void> => {
         res.status(400).json({ error: error.message });
     }
 };
+
+export const getAll = async (req: Response, res: Response): Promise<void> => {
+    try{
+        const cars = await listCars();
+        res.status(200).json(cars);
+    }catch (error: any){
+        res.status(500).json({ error: error.message });
+    }
+};
--- src/routes/car-routes.ts
+++ src/routes/car-routes.ts
@@ -1,9 +1,10 @@
 import {Router} from "express";
 
-import { create } from "../controllers/car-controller"
+import { create, getAll } from "../controllers/car-controller"
 
 const router = Router();
 
 router.post("/", create);
+router.get("/", getAll);
 
 export default router;
